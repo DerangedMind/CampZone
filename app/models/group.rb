04eb226d53f45cfa9ba404c8388b252c6573e1be
Nicole <nicole.tempas@gmail.com>
2017-08-11 19:28:14 +0000

    Add relationships, validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,11 @@
 class Group < ApplicationRecord
+
+  belongs_to :counselor
+  has_many :kids
+
+  validates :camp_id, presence: true
+  validates :name, presence: true
+  validates :min_age, :max_age, presence: true
+  validates :startdate, presence: true
+
 end
