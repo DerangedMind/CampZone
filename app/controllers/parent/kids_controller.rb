89ea18d13a0a0348a2ba0a3141f8d43fe9eb28e1
Nicole <nicole.tempas@gmail.com>
2017-08-11 16:07:41 +0000

    Create kids routes

--- app/controllers/parent/kids_controller.rb
+++ app/controllers/parent/kids_controller.rb
@@ -1,10 +1,37 @@
 class Parent::KidsController < ApplicationController
 
+  def index
+    @kids = Kid.all
+  end
+
   def new
+    @kid = Kid.new
   end
 
   def create
+    @kid = Kid.new(kid_params)
+
+    if @kid.save
+      puts "Kid created!"
+      redirect_to
+    else
+      puts "Kid not created."
+      redirect_to
+
+    end
+
   end
 
+  private
+
+  def kid_params
+    params.require(:kid).permit(
+      :first_name,
+      :last_name,
+      :birthdate,
+      :sin,
+      :medicare_number
+    )
+  end
 
 end
