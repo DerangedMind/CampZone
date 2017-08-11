f768e83608ec56f11650ca355e424c81c520544f
Riki <rick.libby.91@gmail.com>
2017-08-11 18:25:54 +0000

    add camp_directors many-to-many table

--- db/migrate/20170811165628_create_camps.rb
+++ db/migrate/20170811165628_create_camps.rb
@@ -6,5 +6,11 @@ class CreateCamps < ActiveRecord::Migration[5.0]
       t.string :phone_number
       t.timestamps
     end
+
+    create_table :camp_directors do |t|
+      t.integer :user_id
+      t.integer :camp_id
+      t.timestamps
+    end
   end
 end
