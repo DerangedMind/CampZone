762f96a471bf1a72fbde010530f1c41d0949a571
Riki <rick.libby.91@gmail.com>
2017-08-11 10:59:51 -0400

    remove counselors from schema
    this is supposed to be on a different branch. the migration doesn't exist here. looks like rails auto generated it based on my db. removing it so that it will get added in another branch.

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 20170810233036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "counselors", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.string   "alias"
-    t.boolean  "training"
-    t.string   "account_status"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
