4e7020c25f6d8cead3f9c19816deef8439ae7427
Riki <rick.libby.91@gmail.com>
2017-08-11 16:36:49 +0000

    added validations for users on creation. display error messages on failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,16 @@ class SessionsController < ApplicationController
     if @user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = @user.id
       redirect_to '/'
+    else
+      flash.now[:error] = "Invalid username or password."
+      render :new
     end
 
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
   def redirect_to_role_portal(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-
+  
   has_secure_password
+
+  validates :first_name, :last_name, 
+              presence: true
+  validates :password,
+              presence: true, length: { minimum: 8 } 
+  validates :email, 
+              presence: true, uniqueness: true
+  before_validation :downcase_email
 
   def self.authenticate_with_credentials(email, password)
     email.strip!
@@ -8,5 +16,11 @@ class User < ApplicationRecord
 
     @user = User.find_by_email(email)
     @user ? @user.authenticate(password) : false
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase! if self.email.present?
   end
 end
