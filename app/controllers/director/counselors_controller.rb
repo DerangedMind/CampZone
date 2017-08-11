62a003aaaf19d29cefd8b2aab76e57e079d68303
Garo-A <garo.assilian@gmail.com>
2017-08-11 20:15:44 +0000

    Added director ability to edit counselor information

--- app/controllers/director/counselors_controller.rb
+++ app/controllers/director/counselors_controller.rb
@@ -1,21 +1,46 @@
 class Director::CounselorsController < ApplicationController
 
+  def index
+    @counselors = Counselor.all
+  end
+
   def new
-    @counselor = User.new
+    @user = User.new
   end
 
   def create
-    @counselor = User.new(counselor_params)
-    @counselor.role = "counselor"
+    @user = User.new(counselor_params)
+    @user.role = "counselor"
 
-    if @counselor.save
+
+    if @user.save
       puts "COUNSELOR USER SAVED"
+      @counselor = Counselor.create(
+        user_id: @user.id,
+        group_id: 0,
+        alias: "none",
+        training: false,
+        account_status: "pending"
+      )
+      if @counselor.save
+        puts "COUNSELOR SAVED"
+      end
     else
       puts "COUNSELOR NOT SAVED"
       redirect_to "/counselors/new"
     end
   end
 
+  def edit
+    @counselor = Counselor.find(params[:id])
+  end
+
+  def update
+    @counselor = Counselor.find(params[:id])
+    @counselor.update(update_params)
+    puts "COUNSELOR UPDATED"
+  end
+  
   private
 
   def counselor_params
@@ -26,5 +51,14 @@ class Director::CounselorsController < ApplicationController
       :password
     )
   end
+
+    def update_params
+      params.require(:counselor).permit(
+        :group_id,
+        :alias,
+        :training,
+        :account_status
+      )
+    end
 
 end
