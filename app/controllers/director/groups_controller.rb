db90e09bfcbd7919f9c9852b5be53b1a59e998a0
Nicole <nicole.tempas@gmail.com>
2017-08-11 18:49:07 +0000

    Add group_params

--- app/controllers/director/groups_controller.rb
+++ app/controllers/director/groups_controller.rb
@@ -1,2 +1,33 @@
 class Director::GroupsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(
+      :camp_id,
+      :name,
+      :min_age,
+      :max_age,
+      :startdate
+    )
+  end
+
 end
