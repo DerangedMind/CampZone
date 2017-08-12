3bfc22e1ab94d7f989d05f2e2b90451a6d0ae55b
Nicole <nicole.tempas@gmail.com>
2017-08-12 17:09:41 +0000

    Add parent_params

--- app/controllers/parent/parents_controller.rb
+++ app/controllers/parent/parents_controller.rb
@@ -1,2 +1,30 @@
 class Parent::ParentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def parent_params
+    params.require(:parent).permit(
+      :user_id,
+      :address,
+      :phone_number
+    )
+
 end
