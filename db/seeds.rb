f434b413a8c77ae7bd3d11e4edf08f230e76c250
Riki <rick.libby.91@gmail.com>
2017-08-12 16:18:20 +0000

    create seed for directors, counselors, and parents

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+director = User.create!(
+  first_name: Faker::Zelda.character,
+  last_name: "of #{Faker::Zelda.location}",
+  email: "admin@couchlyfe.com",
+  password_digest: BCrypt::Password.create('password'),
+  role: "director"
+)
+
+acc_status = ["inactive", "active", "pending"]
+
+3.times do |count|
+  user = User.create!(
+    first_name: Faker::Zelda.character,
+    last_name: "of #{Faker::Zelda.location}",
+    email: "#{Faker::Cat.name}@campers.com",
+    password_digest: BCrypt::Password.create('password'),
+    role: "counselor"
+  )
+
+  counselor = Counselor.create!(
+    user_id: user.id,
+    alias: Faker::Pokemon.name,
+    training: false,
+    account_status: acc_status[count]
+  )
+end
+
+10.times do |count|
+  user = User.create!(
+    first_name: Faker::Lovecraft.deity,
+    last_name: "of #{Faker::Lovecraft.location}",
+    email: "#{Faker::Lovecraft.word}@campers.com",
+    password_digest: BCrypt::Password.create('password'),
+    role: "parent"
+  )
+end
+
+
