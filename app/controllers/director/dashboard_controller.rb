4635fe53846df9962ebdca3633e0a7bc4e74757e
Garo-A <garo.assilian@gmail.com>
2017-08-12 19:44:06 +0000

    Added dashboard index view

--- app/controllers/director/dashboard_controller.rb
+++ app/controllers/director/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Director::DashboardController < ApplicationController
 def index
   @counselor_count = Counselor.count
   @kids_count = Kid.count
-  @gorup_count = Group.count
+  @group_count = Group.count
 end
 
 end
