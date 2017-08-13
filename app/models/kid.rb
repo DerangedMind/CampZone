5adca51cba9ec9a9b5d7cf784f359bd038e10530
Nicole <nicole.tempas@gmail.com>
2017-08-13 19:52:58 +0000

    Kid relationships

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,5 +1,12 @@
 class Kid < ApplicationRecord
 
+  belongs_to :camp
+  belongs_to :counselor
+  belongs_to :group
+  belongs_to :parent
+
+  has_many :parents
+
   validates :first_name, :last_name, presence: true
   validates :birthdate, presence: true
   validates :sin, :medicare, presence: true
