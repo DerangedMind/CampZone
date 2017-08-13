4c47cf97dd069839ea10104023970c6567b022b9
Nicole <nicole.tempas@gmail.com>
2017-08-13 21:03:40 +0000

    Add relationships

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,6 @@
 class Parent < ApplicationRecord
 
-  has_many :kids
+  has_many :kids, through: :kids_parents
 
   validates :user_id, presence: true
   validates :address, :phone_number, presence: true
