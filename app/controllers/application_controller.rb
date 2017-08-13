7b91f338b34a97a6e6b44082fb35bdebd87db8f5
Riki <rick.libby.91@gmail.com>
2017-08-13 22:25:36 +0000

    added path to each scope in routes. d,c,p for director, counselor, parent. also added bootstrap alert when going to the wrong page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
 
   def authorize_director
     unless director?
-      flash[:error] = "Hmm... no chocolate chip cookies here!"
+      flash[:notice] = "Hmm... no chocolate chip cookies here!"
       redirect_to '/login'
       false
     end
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
 
   def authorize_counselor
     unless counselor?
-      flash[:error] = "I knew I took a wrong turn at Albuquerque..."
+      flash[:notice] = "I knew I took a wrong turn at Albuquerque..."
       redirect_to '/login'
       false
     end
@@ -45,7 +45,7 @@ class ApplicationController < ActionController::Base
 
   def authorize_parent
     unless parent?
-      flash[:error] = "You must have created life to access this point."
+      flash[:notice] = "You must have created life to access this point."
       redirect_to '/login'
       false
     end
