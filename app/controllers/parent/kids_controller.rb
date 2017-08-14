baeeec5cc36f167ec1381b7982d7bedea54511aa
Nicole <nicole.tempas@gmail.com>
2017-08-14 16:43:23 +0000

    Parents and Kids Views

--- app/controllers/parent/kids_controller.rb
+++ app/controllers/parent/kids_controller.rb
@@ -1,19 +1,34 @@
 class Parent::KidsController < ApplicationController
 
+  def index
+  # List of all a parent's kids.
+  end
+
+  def show
+  # Details of specific kid.
+  end
+
   def new
     @kid = Kid.new
   end
 
   def create
     @kid = Kid.new(kid_params)
-
     if @kid.save
       puts "Kid created!"
     else
       puts "Kid not created."
       redirect_to 'kids/new'
-
     end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
