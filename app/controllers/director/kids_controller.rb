5d638f6b46bec9e69a83bf2ab38089848b383b02
Garo-A <garo.assilian@gmail.com>
2017-08-14 16:57:16 +0000

    Added appropriate controller views. Added form to add a Kid

--- app/controllers/director/kids_controller.rb
+++ app/controllers/director/kids_controller.rb
@@ -1,12 +1,23 @@
 class Director::KidsController < ApplicationController
 
   def index
+    @kids = Kid.all.order(first_name: :desc)
   end
 
   def new
+    @kid = Kid.new
   end
 
   def create
+    @kid = Kid.new(kid_params)
+
+    if @kid.save
+      puts "KID SAVED"
+      redirect_to kid_path(:id => @kid.id)
+    else
+      puts "KID NOT SAVED"
+      redirect_to new_kid_path
+    end
   end
 
   def show
@@ -22,7 +33,17 @@ class Director::KidsController < ApplicationController
   end
 
   private
-  
+
+  def kid_params
+    param.require(:kid).permit(
+      :first_name,
+      :last_name,
+      :birthdate,
+      :sin,
+      :medicare
+    )
+  end
+
 
 
 end
