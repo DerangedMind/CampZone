7ed9f182e1fdf37a141c547ab687f845ff389fa5
Garo-A <garo.assilian@gmail.com>
2017-08-17 15:36:49 +0000

    Added more checkboxes in case kids don't have any conditions, dietary restrictions, or medication

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -3,7 +3,7 @@ class Kid < ApplicationRecord
   has_many :kids_parents
   has_many :parents, through: :kids_parents
 
-  has_one :medicalinfo
+  has_one :medical_info
 
   validates :first_name, :last_name, presence: true
   validates :birthdate, presence: true
