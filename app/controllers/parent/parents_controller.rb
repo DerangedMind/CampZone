0188ef3ccdac24e068f2db36aeda92585f5ddb0d
Riki <rick.libby.91@gmail.com>
2017-08-19 23:54:39 +0000

    fix kids create form to send post request. update parents controller for efficiency if address fails to save.

--- app/controllers/parent/parents_controller.rb
+++ app/controllers/parent/parents_controller.rb
@@ -9,26 +9,31 @@ class Parent::ParentsController < Parent::PortalController
     @parent = Parent.find_by_user_id(current_user.id)
     if @parent
       redirect_to parent_profile_path
-    else
-      @parent = Parent.new
     end
+    @address = Address.new
   end
 
   def create
-    @parent = Parent.new(parent_params)
-    @parent.user_id = current_user.id
-
     @address = Address.new(address_params)
-    @parent.address_id = @address.id
 
-    if @parent.save
-      puts "Parent created!"
-      redirect_to parent_profile_path
+    if @address.save
+      @parent = Parent.new(parent_params)
+      @parent.user_id = current_user.id
+      @parent.address_id = @address.id
+
+      if @parent.save
+        puts "Parent created!"
+        redirect_to parent_profile_path
+      else
+        puts "Parent not created."
+        @address.delete
+        flash[:notice] = @parent.errors.full_messages
+        redirect_to new_parent_parent_path
+      end
     else
-      puts "Parent not created."
+      flash[:notice] = @address.errors.full_messages
       redirect_to new_parent_parent_path
     end
-
   end
 
   def edit
@@ -42,20 +47,20 @@ class Parent::ParentsController < Parent::PortalController
 
   private
 
-  def parent_params
-    params.require(:parent).permit(
-      :phone_number
-    )
-  end
-
   def address_params
-    params.permit(
+    params.require(:address).permit(
       :city,
       :province,
       :country,
       :street_address,
       :apt_number,
       :postal_code
+    )
+  end
+
+  def parent_params
+    params.permit(
+      :phone_number
     )
   end
 end
