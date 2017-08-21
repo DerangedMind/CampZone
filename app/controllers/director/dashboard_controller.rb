ab122d04d01f2afdb38114bb5b04731fd39436be
Nicole <nicole.tempas@gmail.com>
2017-08-21 23:49:28 +0000

    Fixed Dashboard to show only logged in Director info

--- app/controllers/director/dashboard_controller.rb
+++ app/controllers/director/dashboard_controller.rb
@@ -1,10 +1,11 @@
 class Director::DashboardController < Director::PortalController
 
-def index
-  @camps = Camp.all
-  @counselors = Counselor.all
-  @kids = Kid.all
-  @groups = Group.all
-end
+  def index
+    @director = Director.find_by_user_id(current_user.id)
+    @camp = @director.camp
+    @groups = @camp.groups
+    @counselors = Counselor.where("id IN (select counselor_id from counselors_groups where group_id IN (select id from groups where camp_id = ?))", @camp.id)
+    @kids = Kid.where("id IN (select kid_id from groups_kids where group_id IN (select id from groups where camp_id = ?))", @camp.id)
+  end
 
 end
