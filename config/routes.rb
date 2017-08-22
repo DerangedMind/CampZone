f28cb4a4e98736efa58e93bc4f2c1e02aa49ca2e
Riki <rick.libby.91@gmail.com>
2017-08-22 23:59:52 +0000

    clean home page. add new parent splash page for splash#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'splash#index'
+  get 'camps', to: 'splash#show'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create', as: 'post_login'
