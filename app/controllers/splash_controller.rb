dfcb80892b4d40e22979d201aea4e8f182eb4ca9
Riki <rick.libby.91@gmail.com>
2017-08-23 15:10:37 +0000

    clean card templates and render calls

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -4,6 +4,11 @@ class SplashController < ApplicationController
   end
 
   def show
-    @camps = Camp.find_each
+    @camps = Camp.all
+    addresses = @camps.map { 
+      |camp| camp.address_id
+    }
+    @addresses = Address.find(addresses)
+    @directors = User.find_by_role("director")
   end
 end
