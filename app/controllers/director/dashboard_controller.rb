b8ef431dc006743db20c16854c0f2bc7ea7801c2
Nicole Tempas <nicole.tempas@gmail.com>
2017-08-23 14:13:53 -0400

    Remove comment, condense spacing

--- app/controllers/director/dashboard_controller.rb
+++ app/controllers/director/dashboard_controller.rb
@@ -4,16 +4,11 @@ class Director::DashboardController < Director::PortalController
     @director = Director.find_by_user_id(current_user.id)
     @camp = @director.camp
     @groups = @camp.groups
-
     @counselors = []
-
     @groups.each do |group|
       @counselors += group.counselors
     end
-
-    # @counselors = Counselor.where("id IN (select counselor_id from counselors_groups where group_id IN (select id from groups where camp_id = ?))", @camp.id)
-
     @kids = Kid.where("id IN (select kid_id from groups_kids where group_id IN (select id from groups where camp_id = ?))", @camp.id)
   end
-
+  
 end
