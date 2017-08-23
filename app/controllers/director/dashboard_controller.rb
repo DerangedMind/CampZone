6edaaaddd60fc6f63024ce40a94dfa14f49b3ab4
Garo-A <garo.assilian@gmail.com>
2017-08-23 17:36:52 +0000

    Fixed danshboard counselor list

--- app/controllers/director/dashboard_controller.rb
+++ app/controllers/director/dashboard_controller.rb
@@ -5,7 +5,12 @@ class Director::DashboardController < Director::PortalController
     @camp = @director.camp
     @groups = @camp.groups
 
-    @counselors = Counselor.all
+    @counselors = []
+
+    @groups.each do |group|
+      @counselors += group.counselors
+    end
+
     # @counselors = Counselor.where("id IN (select counselor_id from counselors_groups where group_id IN (select id from groups where camp_id = ?))", @camp.id)
 
     @kids = Kid.where("id IN (select kid_id from groups_kids where group_id IN (select id from groups where camp_id = ?))", @camp.id)
